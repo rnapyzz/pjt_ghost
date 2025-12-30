a24fd97a8b276f6770a270ff246d7f2d6f7b2ebd
rnapyzz <4040gold.dev@gmail.com>
2025-12-30 22:13:10 +0900

    feat: setup tailwind and connect frontend to backend

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,11 +1,20 @@
 use axum::{Router, routing::get};
 use tokio::net::TcpListener;
-use tower_http::trace::TraceLayer;
+use tower_http::{
+    cors::{Any, CorsLayer},
+    trace::TraceLayer,
+};
 
 pub async fn run(listener: TcpListener) -> Result<(), std::io::Error> {
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/", get(root))
-        .layer(TraceLayer::new_for_http());
+        .layer(TraceLayer::new_for_http())
+        .layer(cors);
 
     axum::serve(listener, app).await
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch((err) => {
+        console.error(err);
+        setMessage("Error fetchin data");
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-slate-900 flex flex-col items-center justfy-center text-white">
+      <h1 className="text-4xl font-semibold mb-4">my dev training</h1>
+      <div className="p-6 bg-slate-800 rounded-lg shadow-xl border border-slate-700">
+        <p className="text-gray-400 text-sm mb-2">message from backend: </p>
+        <p className="text-2xl font-mono text-green-400">{message}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
