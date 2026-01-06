8f2e450285f1d881067cb9af5a230c15da6f8ade
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-01-06 21:52:19 +0900

    chore: add cargo doc to item module

--- backend/src/domain/items.rs
+++ backend/src/domain/items.rs
@@ -1,12 +1,20 @@
+//! Item（項目） に関するデータとドメインロジックの定義。
+//!
+//! このモジュールは、採算管理の主体となる [Job] を構成する要素である `Item` 構造体と、
+//! それを操作するためのリポジトリインターフェースを提供します。
+
 use anyhow::Result;
 use chrono::{DateTime, NaiveDate, Utc};
 use serde::{Deserialize, Serialize};
 use sqlx::{Type, prelude::FromRow};
 use uuid::Uuid;
 
-// ------------------------------------------
-// 勘定科目のカテゴリ（売上、売上原価、販管費）
-// ------------------------------------------
+/// 「勘定科目」の分類を表します。
+///
+/// `AccountCategory` は本バージョンでは3つを持っています。
+/// - `Sales`: 売上高
+/// - `CostOfSales`: 売上原価
+/// - `Sga`: 販管費 (Selling, General and Administrative Expenses)
 #[derive(Debug, Deserialize, Serialize, Type, Clone, PartialEq)]
 #[serde(rename_all = "snake_case")]
 #[sqlx(type_name = "account_category", rename_all = "snake_case")]
@@ -16,9 +24,10 @@ pub enum AccountCategory {
     Sga,
 }
 
-// ------------------------------------------
-// 勘定科目（マスタ）
-// ------------------------------------------
+/// 「勘定科目」を表します。
+///
+/// 各勘定科目`Account`はそれぞれいずれか1つの [AccountCategory] に分類され、
+/// 各勘定科目には複数の [ItemType] が紐づきます。
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Account {
     pub id: Uuid,
@@ -28,9 +37,11 @@ pub struct Account {
     pub updated_at: DateTime<Utc>,
 }
 
-// ------------------------------------------
-// 項目種別（マスタ）
-// ------------------------------------------
+/// 「項目」の種類を表します。
+///
+/// 一般的に「費目」と称される単位ですが、売上要素の場合もあるので「項目」と表現します。
+/// 勘定科目 [Account] はより会計的な分類にですが、実際の活動場面においてはやや抽象的であるため
+/// 勘定科目を具体化した要素として `ItemType` 設定し、ユーザーにとって実用的な構成にすることをサポートします。
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct ItemType {
     pub id: Uuid,
@@ -40,9 +51,9 @@ pub struct ItemType {
     pub updated_at: DateTime<Utc>,
 }
 
-// ------------------------------------------
-// 計上データ
-// ------------------------------------------
+/// 月次単位の「計上データ」を表します。
+///
+/// 計上月と計上金額のセットデータです。
 #[derive(Debug, Serialize, Deserialize, Clone, FromRow)]
 pub struct Entry {
     pub id: Uuid,
@@ -53,9 +64,11 @@ pub struct Entry {
     pub updated_at: DateTime<Utc>,
 }
 
-// ------------------------------------------
-// 項目エンティティ
-// ------------------------------------------
+/// 採算管理の最小単位となる「項目」を表します。
+///
+/// 1つの `Item` は、複数の月次計上データである [Entry] を保持します。
+/// [ItemRepository::update] を使用することで、子要素 [Entry] の洗い替え更新が可能です。
+/// （子要素 [Entry] の更新は洗い替え方式のみ）
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Item {
     pub id: Uuid,
@@ -71,11 +84,20 @@ pub struct Item {
 
 #[async_trait::async_trait]
 pub trait ItemRepository: Send + Sync {
-    // マスタ取得系メソッド
     async fn list_accounts(&self) -> Result<Vec<Account>>;
     async fn list_item_types(&self, account_id: Option<Uuid>) -> Result<Vec<ItemType>>;
 
-    // データ操作系メソッド
+    /// 新しい [Item] を作成します。
+    ///
+    /// # Arguments
+    ///
+    /// * `job_id` - 紐づける [Job] のID
+    /// * `entries` - 初回作成時の月次計上データリスト（空も可）
+    ///
+    /// # Errors
+    ///
+    /// データベースへの接続に失敗した場合や、外部キー制約違反（存在しない `jonb_id` 指定など）
+    /// の場合に [sqlx::Error] をラップしたエラーを返します。
     async fn create(
         &self,
         job_id: Uuid,
