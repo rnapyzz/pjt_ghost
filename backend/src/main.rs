9afe3aec9527a52534c2bdb6313d4614d761f7ed
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-01-11 23:29:59 +0900

    サーバーのベースを作成

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::{Ipv4Addr, SocketAddrV4};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    let app = Router::new().route("/", get(|| async { "Ghost API v2" }));
+    let addr = SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), 3000);
+    let listener = TcpListener::bind(addr).await?;
+
+    tracing::info!("Server listening on {}", addr);
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
