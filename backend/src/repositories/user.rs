7bc14448cf8021c7d1162adedf2553d69fc62a7d
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-01-12 11:48:14 +0900

    UserRepositoryのcreateを実装

---
+++ backend/src/repositories/user.rs
@@ -0,0 +1,69 @@
+use argon2::{
+    Argon2,
+    password_hash::{PasswordHasher, SaltString, rand_core::OsRng},
+};
+use sqlx::PgPool;
+use uuid::Uuid;
+
+use crate::{
+    domains::user::{CreateUserParam, User, UserRepository},
+    error::AppError,
+};
+
+/// UserRepositoryの実装構造体
+#[derive(Debug, Clone)]
+pub struct UserRepositoryImpl {
+    pool: PgPool,
+}
+
+impl UserRepositoryImpl {
+    pub fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait::async_trait]
+impl UserRepository for UserRepositoryImpl {
+    async fn create(&self, params: CreateUserParam) -> Result<User, AppError> {
+        let salt = SaltString::generate(&mut OsRng);
+        let argon2 = Argon2::default();
+        let password_hash = argon2
+            .hash_password(params.password.as_bytes(), &salt)
+            .map_err(|e| anyhow::anyhow!("Password hashing failed: {}", e))?
+            .to_string();
+
+        let role_str = params.role.unwrap_or_default().as_str();
+
+        let user = sqlx::query_as!(
+            User,
+            r#"
+            INSERT INTO users (
+                employee_id,
+                username,
+                name,
+                email,
+                password_hash,
+                role
+            )
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *
+            "#,
+            params.employee_id,
+            params.username,
+            params.name,
+            params.email,
+            password_hash,
+            role_str
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(user)
+    }
+    async fn find_by_id(&self, _id: Uuid) -> Result<Option<User>, AppError> {
+        todo!();
+    }
+    async fn find_by_email(&self, _email: &str) -> Result<Option<User>, AppError> {
+        todo!();
+    }
+}
