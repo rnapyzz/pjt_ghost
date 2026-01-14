3f42a9ee6e03f4794d2eedb972cdee522b795dc5
rnapyzz <96988008+rnapyzz@users.noreply.github.com>
2026-01-14 09:11:15 +0900

    feat: サインアップページを作成

---
+++ frontend/src/pages/Singup.tsx
@@ -0,0 +1,72 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
+
+export function Signup() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-50">
+      <Card className="w-140">
+        <CardHeader>
+          <CardTitle>Create an account</CardTitle>
+          <CardDescription>
+            Enter your information to create your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="employee_id">Employee ID</Label>
+              <Input id="employee_id" placeholder="5桁の数字を入力" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="login_id">Login ID</Label>
+              <Input id="login_id" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" required />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="****@example.com"
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Create account
+            </Button>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-center">
+          <p className="text-sm text-slate-500">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-600 hover:underline">
+              Login
+            </Link>
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
